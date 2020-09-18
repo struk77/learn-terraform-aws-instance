7738bf2620ff4889abf9495933a49fdc4254b9b1
struk77 <struk@yandex.ru>
2020-09-18 17:05:55 +0300

    First working config

--- main.tf
+++ main.tf
@@ -12,7 +12,11 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "${data.aws_ami.latest_ubuntu.id}"
+resource "aws_instance" "www-example" {
+  ami           = "ami-0c960b947cbb2dd16"
   instance_type = "t2.micro"
+  key_name = "deployer"
+  provisioner "local-exec" {
+    command = "echo '        ansible_host: ${aws_instance.www-example.public_ip}' >> provisioning/hosts.yml; sleep 30; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ~/.ssh/deployer.pem -i provisioning/hosts.yml  provisioning/site.yml"
+  }
 }
