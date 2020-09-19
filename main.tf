ad9ae5dd73d51739e47f3ab16a7ae155d1505234
struk <epicwn@aol.com>
2020-09-19 10:01:16 +0300

    Automatically check latest version of ubuntu ami. Create file {host}.yml

--- main.tf
+++ main.tf
@@ -12,11 +12,31 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    # values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "www-example" {
-  ami           = "ami-0c960b947cbb2dd16"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  key_name = "deployer"
+  key_name = "awskey"
   provisioner "local-exec" {
-    command = "echo '        ansible_host: ${aws_instance.www-example.public_ip}' >> provisioning/hosts.yml; sleep 30; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ~/.ssh/deployer.pem -i provisioning/hosts.yml  provisioning/site.yml"
+    command = "bash create-host.sh ${aws_instance.www-example.public_ip} >> ${aws_instance.www-example.id}.yml"
+  }
+  provisioner "local-exec" {
+    command = "sleep 10; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ~/.ssh/awskey.pem -i '${aws_instance.www-example.id}.yml'  provisioning/site.yml; sleep 20; rm -rf '${aws_instance.www-example.id}.yml'"
   }
 }
